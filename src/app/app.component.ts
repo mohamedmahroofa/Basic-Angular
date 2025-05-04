1efdddb59d32c1bb8bd28246e5f4c76536a2d659
mohamedmahroofa <ahm.sheedh@gmail.com>
2025-05-04 18:03:43 +0530

    Added user-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HelloWorldComponent],
+  imports: [HelloWorldComponent, UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'my-first-app';
-}
+export class AppComponent {}
