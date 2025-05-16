475883b362c11664008a6b0f516bd2cda518f0f3
mohamedmahroofa <ahm.sheedh@gmail.com>
2025-05-16 23:20:25 +0530

    Created a new Component called user-card and implemented the profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from './user-card/user-card.component';
 // import { RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/user-card/user-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.css',
+})
+export class UserCardComponent {
+  userName: string = 'Ahmed Sheedh Mohamed Mahroof';
+
+  showMessage() {
+    alert(`Hello ${this.userName}. This is your Profile`);
+  }
+}
